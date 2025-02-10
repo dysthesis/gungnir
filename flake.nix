15e20004b1a170ffa3be8569c0a5b37d57e36b4d
Dysthesis <antheoraviel@protonmail.com>
2025-02-10 20:37:44 +1100

    Changes to the flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,49 @@
 {
-  description = "A very basic flake";
-
+  description = "A flake of power.";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Personal library
+    babel = {
+      url = "github:dysthesis/babel";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    babel,
+    nixpkgs,
+    treefmt-nix,
+    ...
+  }: let
+    inherit (builtins) mapAttrs;
+    inherit (babel) mkLib;
+    lib = mkLib nixpkgs;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    # Systems to support
+    systems = [
+      "aarch64-linux"
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = lib.babel.forAllSystems systems;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+    treefmt = forAllSystems (pkgs: treefmt-nix.lib.evalModule pkgs ./nix/formatters);
+  in
+    # Budget flake-parts
+    mapAttrs (_: val: forAllSystems val) {
+      devShells = pkgs: {default = import ./nix/shell pkgs;};
+      # for `nix fmt`
+      formatter = pkgs: treefmt.${pkgs.system}.config.build.wrapper;
+      # for `nix flake check`
+      checks = pkgs: {
+        formatting = treefmt.${pkgs.system}.config.build.check self;
+      };
+    };
 }
