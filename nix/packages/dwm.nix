d095e470be8a614e897e63d7721a908240a1769c
Dysthesis <antheoraviel@protonmail.com>
2025-02-10 21:12:05 +1100

    Modify derivation to apply all patches on the patches/ directory
    The single_tagset patch for dwm doesn't work yet, need to adjust it

--- nix/packages/dwm.nix
+++ nix/packages/dwm.nix
@@ -1,15 +1,34 @@
 {
   self,
+	lib,
   stdenv,
   libX11,
   libXinerama,
   libXft,
-}:
-stdenv.mkDerivation {
+}: let
+	inherit
+		(builtins)
+		readDir
+		attrNames
+		map
+		;
+	inherit
+    (lib)
+    filterAttrs
+    ;
+in stdenv.mkDerivation rec {
   pname = "dwm";
   version = "6.5";
 
   src = "${self}/src/dwm";
+
+	patches = let
+		patchesDir = "${src}/patches";
+	in patchesDir
+			|> readDir
+			|> filterAttrs (_name: value: value == "regular")
+			|> attrNames
+			|> (xs: map (x: "${patchesDir}/${x}") xs);
 
   strictDeps = true;
 
