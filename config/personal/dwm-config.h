0cd4b24ba75d9ace68e3c5344a80d16820e74345
Dysthesis <antheoraviel@protonmail.com>
2025-02-11 20:27:05 +1100

    fix window swallowing (hopefully)

--- config/personal/dwm-config.h
+++ config/personal/dwm-config.h
@@ -72,7 +72,7 @@ static const Rule rules[] = {
        noswallow  floatpos  monitor */
     {"Gimp", NULL, NULL, 0, 1, 0, 0, NULL, -1},
     {"Firefox", NULL, NULL, 1 << 8, 0, 0, -1, NULL, -1},
-    {"St", NULL, NULL, 0, 0, 1, 0, NULL, -1},
+    {"st-256color", NULL, NULL, 0, 0, 1, 0, NULL, -1},
     {NULL, "term", NULL, SPTAG(0), 1, 0, 0, NULL, -1},
     {NULL, "notes", NULL, SPTAG(1), 1, 0, 0, NULL, -1},
     {NULL, "signal", NULL, SPTAG(2), 1, 0, 0, "50% 50% 1805W 1203H", -1},
