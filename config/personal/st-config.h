94fa1c3960b3cb186607d6702016242a5223ecde
Dysthesis <antheoraviel@protonmail.com>
2025-02-12 09:12:59 +1100

    Increase st font size

--- config/personal/st-config.h
+++ config/personal/st-config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font =
-    "JBMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+    "JBMono Nerd Font:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 20;
 
 /*
