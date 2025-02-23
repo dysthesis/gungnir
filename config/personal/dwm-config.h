cdc3ad3d34d5813df649d1ed3505dee7a918b620
Dysthesis <antheoraviel@protonmail.com>
2025-02-23 15:20:07 +1100

    Add shortcut for xsecurelock

--- config/personal/dwm-config.h
+++ config/personal/dwm-config.h
@@ -140,6 +140,7 @@ static const Key keys[] = {
     {0, XF86XK_MonBrightnessDown, spawn, {.v = downbright}},
     {MODKEY, XK_r, spawn, {.v = dmenucmd}},
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY | ShiftMask, XK_Escape, spawn, SHCMD("xsecurelock")},
     /*{MODKEY, XK_b, togglebar, {0}},*/
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
