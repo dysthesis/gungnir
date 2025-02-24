441bc424f2d8c15c7cf280975ece48c989ea968c
Dysthesis <antheoraviel@protonmail.com>
2025-02-24 17:51:19 +1100

    Nixify configs

---
+++ config/personal/dmenu.nix
@@ -0,0 +1,49 @@
+{
+  pkgs,
+  config ? {
+    font = "JBMono Nerd Font";
+    fontSize = 12;
+    lineHeight = 32;
+  },
+  ...
+}: let
+  inherit (pkgs) writeText;
+in
+  with config;
+    writeText "config.h"
+    /*
+    C
+    */
+    ''
+      /* See LICENSE file for copyright and license details. */
+      /* Default settings; can be overriden by command line. */
+
+      static int instant =
+          1;                 /* -n  option; if 1, select single entry automatically */
+      static int topbar = 0; /* -b  option; if 0, dmenu appears at bottom     */
+      static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+      /* -fn option overrides fonts[0]; default X11 font or font set */
+      static const char *fonts[] = {
+          "${font}:pixelsize=${toString fontSize}:antialias=true:autohint=true"};
+      static const char *prompt =
+          NULL; /* -p  option; prompt to the left of input field */
+      static const char *colors[][2] = {
+          /*     fg         bg       */
+          [SchemeNorm] = {"#ffffff", "#000000"},
+          [SchemeSel] = {"#708090", "#555555"},
+          [SchemeSelHighlight] = {"#7788AA", "#555555"},
+          [SchemeNormHighlight] = {"#7788AA", "#000000"},
+          [SchemeOut] = {"#000000", "#00ffff"},
+      };
+      /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+      static unsigned int lines = 0;
+      /* -h option; minimum height of a menu line */
+      static unsigned int lineheight = ${toString lineHeight};
+      static unsigned int min_lineheight = ${toString lineHeight};
+
+      /*
+       * Characters not considered part of a word while deleting words
+       * for example: " /?\"&[]"
+       */
+      static const char worddelimiters[] = " ";
+    ''
--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -3,24 +3,189 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  stConfig = fontSize: {
+    inherit fontSize;
+    font = "JBMono Nerd Font";
+    borderpx = 30;
+    fontScale = {
+      width = 1.0;
+      height = 1.2;
+    };
+    colours = {
+      normal = {
+        black = "#080808";
+        red = "#D70000";
+        green = "#789978";
+        orange = "#FFAA88";
+        blue = "#7788AA";
+        magenta = "#D7007D";
+        cyan = "#708090";
+        white = "#DEEEED";
+      };
+      bright = {
+        black = "#444444";
+        red = "#D70000";
+        green = "#789978";
+        orange = "#FFAA88";
+        blue = "#7788AA";
+        magenta = "#D7007D";
+        cyan = "#708090";
+        white = "#DEEEED";
+      };
+      foreground = "#FFFFFF";
+      background = "#000000";
+      highlight = "#F5E0DC";
+    };
+  };
+  dwmConfig = fontSize: {
+    borderpx = 1;
+    topbar = 0;
+    font = "JBMono Nerd Font";
+    inherit fontSize;
+    colours = {
+      gray1 = "#080808";
+      gray2 = "#191919";
+      gray3 = "#2A2A2A";
+      gray4 = "#444444";
+      cyan = "#708090";
+      white = "#ffffff";
+    };
+    scratchpads = [
+      rec {
+        name = "term";
+        class = name;
+        isTerm = 1;
+        cmd = [
+          "st"
+          "-n"
+          "${class}"
+          "-g"
+          "190x40"
+        ];
+        tag = 0;
+        floatpos = null;
+        key = "t";
+      }
+      rec {
+        name = "btop";
+        class = name;
+        isTerm = 1;
+        cmd = [
+          "st"
+          "-n"
+          "${class}"
+          "-g"
+          "190x40"
+          "-e"
+          "btop"
+        ];
+        tag = 1;
+        floatpos = null;
+        key = "b";
+      }
+      rec {
+        name = "task";
+        class = name;
+        isTerm = 1;
+        cmd = [
+          "st"
+          "-n"
+          "${class}"
+          "-g"
+          "190x40"
+          "-e"
+          "taskwarrior-tui"
+        ];
+        tag = 2;
+        floatpos = null;
+        key = "d";
+      }
+      rec {
+        name = "spotify";
+        class = name;
+        isTerm = 1;
+        cmd = [
+          "st"
+          "-n"
+          "${class}"
+          "-g"
+          "190x40"
+          "-e"
+          "spotify_player"
+        ];
+        tag = 3;
+        floatpos = null;
+        key = "m";
+      }
+      rec {
+        name = "notes";
+        class = name;
+        isTerm = 1;
+        cmd = [
+          "st"
+          "-n"
+          "${class}"
+          "-g"
+          "190x40"
+          "-e"
+          "sh"
+          "-c"
+          "tmux new-session -As Notes -c ~/Documents/Notes/Contents 'direnv exec . nvim'"
+        ];
+        tag = 4;
+        floatpos = null;
+        key = "n";
+      }
+      {
+        name = "signal_desktop";
+        class = "signal";
+        isTerm = 0;
+        cmd = [
+          "signal-desktop"
+        ];
+        tag = 5;
+        floatpos = "50% 50% 1805W 1203H";
+        key = "s";
+      }
+    ];
+  };
+  dmenuConfig = fontSize: {
+    inherit fontSize;
+    font = "JBMono Nerd Font";
+    lineHeight = 32;
+  };
+in {
   # Default configurations
   dwm-default = pkgs.callPackage ./dwm.nix {inherit self lib;};
   dmenu-default = pkgs.callPackage ./dmenu.nix {inherit self lib;};
   st-default = pkgs.callPackage ./st.nix {inherit self lib;};
 
   # Personal configurations
-  dwm = pkgs.callPackage ./dwm.nix {
-    inherit self lib;
-    config-file = builtins.trace "${import ../../config/personal/dwm.nix {inherit lib pkgs;}}" import ../../config/personal/dwm.nix {inherit lib pkgs;};
-  };
-  dmenu = pkgs.callPackage ./dmenu.nix {
-    inherit self lib;
-    config-file = "${self}/config/personal/dmenu-config.h";
-  };
-  st = pkgs.callPackage ./st.nix {
-    inherit self lib;
-    config-file = "${self}/config/personal/st-config.h";
-  };
+  dwm = {fontSize ? 12}:
+    pkgs.callPackage ./dwm.nix {
+      inherit self lib;
+      config-file = import ../../config/personal/dwm.nix {
+        inherit lib pkgs;
+        config = dwmConfig fontSize;
+      };
+    };
+  dmenu = {fontSize ? 12}:
+    pkgs.callPackage ./dmenu.nix {
+      inherit self lib;
+      config-file = import ../../config/personal/dmenu.nix {
+        inherit lib pkgs;
+        config = dmenuConfig fontSize;
+      };
+    };
+  st = {fontSize ? 12}:
+    pkgs.callPackage ./st.nix {
+      inherit self lib;
+      config-file = import ../../config/personal/st.nix {
+        inherit lib pkgs;
+        config = stConfig fontSize;
+      };
+    };
+
   dwm-bar = pkgs.callPackage ./dwm-bar.nix {inherit pkgs lib;};
 }
