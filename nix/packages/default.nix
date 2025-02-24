141bbbfe02531b0bce04b448cb93ea06b172ef66
Dysthesis <antheoraviel@protonmail.com>
2025-02-25 09:38:28 +1100

    Parametrise everything

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -4,54 +4,27 @@
   pkgs,
   ...
 }: let
-  stConfig = fontSize: {
-    inherit fontSize;
-    font = "JBMono Nerd Font";
-    borderpx = 30;
-    fontScale = {
-      width = 1.0;
-      height = 1.2;
-    };
-    colours = {
-      normal = {
-        black = "#080808";
-        red = "#D70000";
-        green = "#789978";
-        orange = "#FFAA88";
-        blue = "#7788AA";
-        magenta = "#D7007D";
-        cyan = "#708090";
-        white = "#DEEEED";
-      };
-      bright = {
-        black = "#444444";
-        red = "#D70000";
-        green = "#789978";
-        orange = "#FFAA88";
-        blue = "#7788AA";
-        magenta = "#D7007D";
-        cyan = "#708090";
-        white = "#DEEEED";
-      };
-      foreground = "#FFFFFF";
-      background = "#000000";
-      highlight = "#F5E0DC";
-    };
-  };
-  dwmConfig = fontSize: {
-    borderpx = 1;
-    topbar = 0;
-    font = "JBMono Nerd Font";
-    inherit fontSize;
-    colours = {
+in {
+  # Default configurations
+  dwm-default = pkgs.callPackage ./dwm.nix {inherit self lib;};
+  dmenu-default = pkgs.callPackage ./dmenu.nix {inherit self lib;};
+  st-default = pkgs.callPackage ./st.nix {inherit self lib;};
+
+  # Personal configurations
+  dwm = {
+    borderpx ? 1,
+    topbar ? 0,
+    font ? "JBMono Nerd Font",
+    fontSize ? 12,
+    colours ? {
       gray1 = "#080808";
       gray2 = "#191919";
       gray3 = "#2A2A2A";
       gray4 = "#444444";
       cyan = "#708090";
       white = "#ffffff";
-    };
-    scratchpads = [
+    },
+    scratchpads ? [
       rec {
         name = "term";
         class = name;
@@ -148,42 +121,67 @@
         floatpos = "50% 50% 1805W 1203H";
         key = "s";
       }
-    ];
-  };
-  dmenuConfig = fontSize: {
-    inherit fontSize;
-    font = "JBMono Nerd Font";
-    lineHeight = 32;
-  };
-in {
-  # Default configurations
-  dwm-default = pkgs.callPackage ./dwm.nix {inherit self lib;};
-  dmenu-default = pkgs.callPackage ./dmenu.nix {inherit self lib;};
-  st-default = pkgs.callPackage ./st.nix {inherit self lib;};
-
-  # Personal configurations
-  dwm = {fontSize ? 12}:
+    ],
+  }:
     pkgs.callPackage ./dwm.nix {
       inherit self lib;
       config-file = import ../../config/personal/dwm.nix {
         inherit lib pkgs;
-        config = dwmConfig fontSize;
+        config = {inherit borderpx topbar font fontSize colours scratchpads;};
       };
     };
-  dmenu = {fontSize ? 12}:
+  dmenu = {
+    fontSize ? 12,
+    font ? "JBMono Nerd Font",
+    lineHeight ? 32,
+  }:
     pkgs.callPackage ./dmenu.nix {
       inherit self lib;
       config-file = import ../../config/personal/dmenu.nix {
         inherit lib pkgs;
-        config = dmenuConfig fontSize;
+        config = {inherit fontSize font lineHeight;};
       };
     };
-  st = {fontSize ? 12}:
+
+  st = {
+    fontSize ? 12,
+    font ? "JBMono Nerd Font",
+    borderpx ? 30,
+    fontScale ? {
+      width = 1.0;
+      height = 1.2;
+    },
+    colours ? {
+      normal = {
+        black = "#080808";
+        red = "#D70000";
+        green = "#789978";
+        orange = "#FFAA88";
+        blue = "#7788AA";
+        magenta = "#D7007D";
+        cyan = "#708090";
+        white = "#DEEEED";
+      };
+      bright = {
+        black = "#444444";
+        red = "#D70000";
+        green = "#789978";
+        orange = "#FFAA88";
+        blue = "#7788AA";
+        magenta = "#D7007D";
+        cyan = "#708090";
+        white = "#DEEEED";
+      };
+      foreground = "#FFFFFF";
+      background = "#000000";
+      highlight = "#F5E0DC";
+    },
+  }:
     pkgs.callPackage ./st.nix {
       inherit self lib;
       config-file = import ../../config/personal/st.nix {
         inherit lib pkgs;
-        config = stConfig fontSize;
+        config = {inherit fontSize fontScale font borderpx colours;};
       };
     };
 
