95848d84695b519f36cc488335dd65e949482d75
Dysthesis <antheoraviel@protonmail.com>
2025-03-25 12:03:28 +1100

    Typo

--- config/personal/dwl.nix
+++ config/personal/dwl.nix
@@ -183,7 +183,7 @@ in
     	{ MODKEY,                    XKB_KEY_t,      focusortogglematchingscratch, {.v = termscratch} },
     	{ MODKEY,                    XKB_KEY_n,      focusortogglematchingscratch, {.v = notescratch} },
     	{ MODKEY,                    XKB_KEY_s,      focusortogglematchingscratch, {.v = signalscratch} },
-    	{ MODKEY,                    XKB_KEY_b,      focusortogglematchingscratch, {.v = signalscratch} },
+    	{ MODKEY,                    XKB_KEY_b,      focusortogglematchingscratch, {.v = btopscratch} },
     	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
     	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
     	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_J,          movestack,      {.i = +1} },
